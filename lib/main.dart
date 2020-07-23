95464e22febeeefb1facd55addde40ac1cb03bd6
wilder <robert.g.wilder@gmail.com>
2020-07-23 20:30:13 +0100

    button function now working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,20 +16,44 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 2;
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-//          flex: 2,
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-//          flex: 1,
-          child: Image.asset('images/dice2.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+//          Button One
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDice = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dice$leftDice.png'),
+            ),
+          ),
+          Expanded(
+//          Button 2
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  rightDice = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dice$rightDice.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
